0760d7c0351b675b03aaa2a52bb9b2acdf77836d
wh75er <wh75er@gmail.com>
2018-09-20 02:58:24 +0300

    changed structure of programs

--- lab_01/damerau_levenstain.c
+++ lab_01/damerau_levenstain.c
@@ -6,42 +6,16 @@
 
 int inputData( char* s1, char* s2, int* err );
 int levensteinDistance( char* s1, char *s2, 
-                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE], 
-                        int* err );
+                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE] );
 
 void printMatrix( int m[MAX_STRING_SIZE][MAX_STRING_SIZE],
-                    int len1, int len2 )
-{
-    for(int i = 0; i <= len1; i++) {
-        printf("\n");
-        for(int j = 0; j <= len2; j++)
-            printf("%d ", m[i][j]);
-    }
-}
-
+                                                int len1, int len2 );
 void matrixInit( int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE], 
-                int a, int b )
-{
-    for(; a >= 0; a--)
-        (*matrix)[a][0] = a;
-    for(; b > 0; b--)
-        (*matrix)[0][b] = b;
-}
+                                                    int a, int b );
+int min(int count, ...);
 
-int min(int count, ...) {
-    va_list ap;
 
-    va_start(ap, count);
-    int min = va_arg(ap, int);
-    for(int i = 0; i < count-1; i++) {
-        int x = va_arg(ap, int);
-        if(x < min)
-            min = x;
-    }
-    va_end(ap);
-    
-    return min; 
-}
+
 
 int main() {
         int err = 0;
@@ -51,25 +25,15 @@ int main() {
         inputData(s1, s2, &err);
 
         int matrix[MAX_STRING_SIZE][MAX_STRING_SIZE];
-        levensteinDistance(s1, s2, &matrix, &err);
+        levensteinDistance(s1, s2, &matrix);
 
-        //printf("%s\n%s\n", s1, s2);
+        printMatrix(matrix, strlen(s1), strlen(s2));
 }
 
-int inputData( char* s1, char* s2, int* err )
-{
-    printf(" Input s1: ");
-    scanf("%s", s1);
 
-    printf(" Input s2: ");
-    scanf("%s", s2);
-
-    return 1;
-}
 
 int levensteinDistance( char* s1, char* s2, 
-                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE],
-                        int* err )
+                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE] )
 {
     size_t len1 = strlen(s1);
     size_t len2 = strlen(s2);
@@ -94,6 +58,50 @@ int levensteinDistance( char* s1, char* s2,
                     l2 = (*matrix)[i-2][j-2]+1;
             (*matrix)[i][j] = min(2, l1, l2);
         }
+}
+
+
+int inputData( char* s1, char* s2, int* err )
+{
+    printf(" Input s1: ");
+    scanf("%s", s1);
+
+    printf(" Input s2: ");
+    scanf("%s", s2);
+
+    return 1;
+}
+
+int min(int count, ...) {
+    va_list ap;
+
+    va_start(ap, count);
+    int min = va_arg(ap, int);
+    for(int i = 0; i < count-1; i++) {
+        int x = va_arg(ap, int);
+        if(x < min)
+            min = x;
+    }
+    va_end(ap);
     
-    printMatrix(*matrix, len1, len2);
+    return min; 
+}
+
+void printMatrix( int m[MAX_STRING_SIZE][MAX_STRING_SIZE],
+                    int len1, int len2 )
+{
+    for(int i = 0; i <= len1; i++) {
+        printf("\n");
+        for(int j = 0; j <= len2; j++)
+            printf("%d ", m[i][j]);
+    }
+}
+
+void matrixInit( int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE], 
+                int a, int b )
+{
+    for(; a >= 0; a--)
+        (*matrix)[a][0] = a;
+    for(; b > 0; b--)
+        (*matrix)[0][b] = b;
 }
--- lab_01/levenstein.c
+++ lab_01/levenstein.c
@@ -6,8 +6,81 @@
 
 int inputData( char* s1, char* s2, int* err );
 int levensteinDistance( char* s1, char *s2, 
-                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE], 
-                        int* err );
+                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE] );
+
+void printMatrix( int m[MAX_STRING_SIZE][MAX_STRING_SIZE],
+                                                int len1, int len2 );
+void matrixInit( int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE], 
+                                                    int a, int b );
+int min(int count, ...);
+
+
+
+
+int main() {
+        int err = 0;
+
+        char s1[MAX_STRING_SIZE-1] = "";
+        char s2[MAX_STRING_SIZE-1] = ""; 
+        inputData(s1, s2, &err);
+
+        int matrix[MAX_STRING_SIZE][MAX_STRING_SIZE];
+        levensteinDistance(s1, s2, &matrix);
+
+        printMatrix(matrix, strlen(s1), strlen(s2));
+}
+
+
+
+int levensteinDistance( char* s1, char* s2, 
+                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE] )
+{
+    size_t len1 = strlen(s1);
+    size_t len2 = strlen(s2);
+
+    matrixInit(matrix, len1, len2);
+
+    int cost = 1; 
+    for(int i = 1; i <= len1; i++)
+        for(int j = 1; j <= len2; j++) {
+            int flag = 0;
+            if(s1[i-1] == s2[j-1])
+                cost = 0;
+            else
+                cost = 1;
+            (*matrix)[i][j] = min( 3,
+                        (*matrix)[i-1][j]+1, 
+                        (*matrix)[i][j-1]+1,
+                        (*matrix)[i-1][j-1]+cost );
+        }
+}
+
+
+int inputData( char* s1, char* s2, int* err )
+{
+    printf(" Input s1: ");
+    scanf("%s", s1);
+
+    printf(" Input s2: ");
+    scanf("%s", s2);
+
+    return 1;
+}
+
+int min(int count, ...) {
+    va_list ap;
+
+    va_start(ap, count);
+    int min = va_arg(ap, int);
+    for(int i = 0; i < count-1; i++) {
+        int x = va_arg(ap, int);
+        if(x < min)
+            min = x;
+    }
+    va_end(ap);
+    
+    return min; 
+}
 
 void printMatrix( int m[MAX_STRING_SIZE][MAX_STRING_SIZE],
                     int len1, int len2 )
@@ -26,69 +99,4 @@ void matrixInit( int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE],
         (*matrix)[a][0] = a;
     for(; b > 0; b--)
         (*matrix)[0][b] = b;
-}
-
-int min(int count, ...) {
-    va_list ap;
-
-    va_start(ap, count);
-    int min = va_arg(ap, int);
-    for(int i = 0; i < count-1; i++) {
-        int x = va_arg(ap, int);
-        if(x < min)
-            min = x;
-    }
-    va_end(ap);
-    
-    return min; 
-}
-
-int main() {
-        int err = 0;
-
-        char s1[MAX_STRING_SIZE-1] = "";
-        char s2[MAX_STRING_SIZE-1] = ""; 
-        inputData(s1, s2, &err);
-
-        int matrix[MAX_STRING_SIZE][MAX_STRING_SIZE];
-        levensteinDistance(s1, s2, &matrix, &err);
-
-        //printf("%s\n%s\n", s1, s2);
-}
-
-int inputData( char* s1, char* s2, int* err )
-{
-    printf(" Input s1: ");
-    scanf("%s", s1);
-
-    printf(" Input s2: ");
-    scanf("%s", s2);
-
-    return 1;
-}
-
-int levensteinDistance( char* s1, char* s2, 
-                        int (*matrix)[MAX_STRING_SIZE][MAX_STRING_SIZE],
-                        int* err )
-{
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);
-
-    matrixInit(matrix, len1, len2);
-
-    int cost = 1; 
-    for(int i = 1; i <= len1; i++)
-        for(int j = 1; j <= len2; j++) {
-            int flag = 0;
-            if(s1[i-1] == s2[j-1])
-                cost = 0;
-            else
-                cost = 1;
-            (*matrix)[i][j] = min( 3,
-                                 (*matrix)[i-1][j]+1, 
-                                 (*matrix)[i][j-1]+1,
-                                 (*matrix)[i-1][j-1]+cost );
-        }
-    
-    printMatrix(*matrix, len1, len2);
 }
