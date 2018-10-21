dbad3e1d8f9afc4f24607b21f0c2a99c4fac7618
kam16u810 <bokseng12@gmail.com>
2018-10-21 18:18:17 +0300

    add sort functions

--- lab_03/functions.c
+++ lab_03/functions.c
@@ -1,119 +1,89 @@
 #include "functions.h"
 
-void winograd(  int** a, int r_a, int c_a , 
-                int** b, int r_b, int c_b, 
-                int** c, int r_c, int c_c,
-                int* rows, int* columns)
+void insert(int* a, size_t size)
 {
-    int d = c_a / 2;
-
-    for(int i = 0; i < r_a; i++) {
-        rows[i] = rows[i] + a[i][0] * a[i][1];
-        for(int j = 1; j < d; j++)
-            rows[i] = rows[i] + a[i][2*j] * a[i][2*j+1];
-    }
-
-    for(int i = 0; i < c_b; i++) {
-        columns[i] = columns[i] + b[0][i]*b[1][i];
-        for(int j = 1; j < d; j++)
-            columns[i] = columns[i] + b[2*j][i] * b[2*j+1][i];
-    }
-
-    for(int i = 0; i < r_a; i++)
-        for(int j = 0; j < c_b; j++) {
-            c[i][j] = -rows[i] - columns[j];
-            for(int k = 0; k < d; k++)
-                c[i][j] = c[i][j] + (a[i][2*k] + b[2*k+1][j]) * 
-                            (a[i][2*k+1] + b[2*k][j]);
-        }
-
-    if(c_a%2)
-        for(int i = 0; i < r_a; i++)
-            for(int j = 0; j < c_b; j++)
-                c[i][j] = c[i][j] + a[i][c_a-1] * b[c_a-1][j];
-}
-
-void winograd_enhanced( int** a, int r_a, int c_a , 
-                        int** b, int r_b, int c_b, 
-                        int** c, int r_c, int c_c,
-                        int* rows, int* columns)
-{
-    int d = c_a - 1;
-    for(int i = 0; i < r_a; i++) {
-        for(int j = 0; j < d; j+=2)
-            rows[i] += a[i][j] * a[i][j+1];
-    }
-
-    for(int i = 0; i < c_b; i++) {
-        for(int j = 0; j < d; j+=2)
-            columns[i] += b[j][i] * b[j+1][i];
-    }
-
-    for(int i = 0; i < r_a; i++)
-        for(int j = 0; j < c_b; j++) {
-            c[i][j] = -rows[i] - columns[j];
-            for(int k = 0; k < d; k+=2)
-                c[i][j] += (a[i][k] + b[k+1][j]) * 
-                            (a[i][k+1] + b[k][j]);
-        }
-
-    if(c_a%2)
-        for(int i = 0; i < r_a; i++)
-            for(int j = 0; j < c_b; j++)
-                c[i][j] += a[i][d] * b[d][j];
-}
-
-void classic(           int** a, int r_a, int c_a , 
-                        int** b, int r_b, int c_b, 
-                        int** c, int r_c, int c_c)
-{
-    for(int i = 0; i < r_a; i++)
-        for(int j = 0; j < c_b; j++)
-            for(int k = 0; k < r_b; k++)
-                c[i][j] += a[i][k] * b[k][j];
-}
-
-
-int** allocateM(size_t r, size_t c)
-{
-    int** matrix = (int**)malloc(sizeof(int*) * r);
-    if(matrix)
-        matrix[0] = (int*)malloc(sizeof(int) * c * r);
-    else
-        return NULL;
-
-    for(int i = 0; i < r; i++)
-        matrix[i] = (*matrix + c * i);
+    int j, tmp;
     
-    for(int i = 0; i < r; i++)
-        for(int j = 0; j < c; j++)
-            matrix[i][j] = 0;
-
-    return matrix;
-}
-
-void randM(int** a, size_t r, size_t c)
-{
-    for(int i = 0; i < r; i++)
-        for(int j = 0; j < c; j++)
-            a[i][j] = rand() % 50;
-}
-
-void printM( int** a, size_t len1, size_t len2 )
-{
-    for(int i = 0; i < len1; i++) {
-        printf("\n");
-        for(int j = 0; j < len2; j++)
-            printf("%d ", a[i][j]);
+    for(int i = 1; i < size; i++) {
+        tmp = a[i];
+        for (j = i - 1; j >= 0 && a[j] > tmp; j--)
+            a[j + 1] = a[j];
+        a[j + 1] = tmp;
     }
 }
 
-void freeM(int** a, size_t r)
+// ---------------------------------------------------------------------------------------------
+
+void merge(int* a, int* l, size_t leftCount, int* r, size_t rightCount)
 {
-    free(a[0]);
-    //for(int i = 0; i < r; i++)
-    //    free(a[i]);
-    free(a);
+	int i = 0, 
+        j = 0, 
+        k = 0;
+
+	while(i<leftCount && j< rightCount) {
+		if(l[i]  < r[j]) a[k++] = l[i++];
+		else a[k++] = r[j++];
+	}
+	while(i < leftCount) a[k++] = l[i++];
+	while(j < rightCount) a[k++] = r[j++];
 }
 
+void mergeSort(int* a, size_t n)
+{
+	int mid, i, *l, *r;
+	if(n < 2) return;
+
+	mid = n/2;
+
+	l = (int*)malloc(mid*sizeof(int)); 
+	r = (int*)malloc((n - mid)*sizeof(int)); 
+	
+	for(i = 0; i<mid; i++) l[i] = a[i]; // creating left subarray
+	for(i = mid; i<n; i++) r[i-mid] = a[i]; // creating right subarray
+
+	mergeSort(l, mid);
+	mergeSort(r, n-mid);
+	merge(a, l, mid, r, n-mid);
+        free(l);
+        free(r);
+}
+
+// ---------------------------------------------------------------------------------------------
+
+void bubble(int* a, size_t size)
+{
+    for(int i = 0; i < size; i++)
+        for(int j = 0; j < size-j-i; j++)
+            if(a[j] > a[j+1])
+                swap(&a[j], &a[j+1]);
+}
+
+// ---------------------------------------------------------------------------------------------
+
+void swap(int* ap, int* bp)
+{
+    int tmp = *ap;
+    *ap = *bp;
+    *bp = tmp;
+}
+
+int* allocateA(size_t size)
+{
+    int* array = (int*)malloc(sizeof(int) * size);
+    
+    return array;
+}
+
+void randA(int* a, size_t size)
+{
+    for(int i = 0; i < size; i++)
+        a[i] = rand() % 50;
+}
+
+void printA(int* a, size_t size) 
+{ 
+    for(int i = 0; i < size; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+} 
 
--- lab_03/functions.h
+++ lab_03/functions.h
@@ -5,23 +5,14 @@
 #include <time.h>
 #include <stdio.h>
 
-void winograd(  int** a, int r_a, int c_a , 
-                int** b, int r_b, int c_b, 
-                int** c, int r_c, int c_c,
-                int* rows, int* columns);
 
-void winograd_enhanced( int** a, int r_a, int c_a , 
-                        int** b, int r_b, int c_b, 
-                        int** c, int r_c, int c_c,
-                        int* rows, int* columns);
+void bubble(int* a, size_t size);
+void mergeSort(int* a, size_t n);
+void insert(int* a, size_t size);
 
-void classic          ( int** a, int r_a, int c_a, 
-                        int** b, int r_b, int c_b, 
-                        int** c, int r_c, int c_c);
-
-int** allocateM(size_t n, size_t m);
-void randM(int** a, size_t r, size_t c);
-void printM( int** a, size_t len1, size_t len2 );
-void freeM(int** a, size_t r);
+int* allocateA(size_t size);
+void swap(int* ap, int* bp);
+void randA(int* a, size_t size);
+void printA(int* a, size_t size);
 
 #endif
