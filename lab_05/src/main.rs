ee4d471e602531bf62012a3da4e84239cbfe17d6
kam16u810 <bokseng12@gmail.com>
2018-12-10 15:25:57 +0300

    add not finished winograd algorithm

--- lab_05/src/main.rs
+++ lab_05/src/main.rs
@@ -8,10 +8,10 @@ use winograd::*;
 fn main() {
     println!("Hello, world!");
     
-    let m1 = create_random_matrix(3, 1);
+    let m1 = create_random_matrix(2, 3);
     println!("This is the first random matrix {:?}", m1);
 
-    let m2 = create_random_matrix(1, 4);
+    let m2 = create_random_matrix(3, 3);
     println!("This is the second random matrix {:?}", m2);
     
     let mut _2d = winograd(m1, m2);
--- lab_05/src/winograd.rs
+++ lab_05/src/winograd.rs
@@ -1,17 +1,56 @@
 pub fn winograd(m1: Vec<Vec<i32>>, m2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let r_m1 = m1.len();
+    let c_m1 = m1[0].len();
+    println!("m1 : {}x{}", r_m1, c_m1);
+    let r_m2 = m2.len();
+    let c_m2 = m2[0].len();
+    println!("m2 : {}x{}", r_m2, c_m2);
+
     let mut c = Vec::new();
 
-    for _i in 0..m1.len() {
+    for _i in 0..r_m1 {
         c.push(Vec::new());
     }
-    let row = &m2[0];
     for mut i in &mut c {
-        for _j in 0..row.len() {
+        for _j in 0..c_m2 {
             i.push(0);
         }
     }
 
-    println!("Hello world");
+    let d = c_m1 / 2;
+
+    let mut rows = vec![0; r_m1];
+    println!("Here's array");
+    for i in 0..r_m1 {
+        rows[i] += m1[i][0] + m1[i][1];
+        for j in 1..d {
+            rows[i] += m1[i][2*j] * m1[i][2*j+1];
+        }
+    }
+    
+    let mut columns = vec![0; c_m2];
+    for i in 0..c_m2 {
+        columns[i] += m2[0][i]*m2[1][i];
+        for j in 1..d {
+            columns[i] += m2[2*j][i]*m2[2*j+1][i];
+        }
+    }
+
+    for i in 0..r_m1 { 
+        for j in 0..c_m2 {
+            c[i][j] = -rows[i] - columns[j];
+            for k in 0..d {
+                c[i][j] += (m1[i][2*k] + m2[2*k+1][j]) *
+                            (m1[i][2*k+1] + m2[2*k][j]);
+            }
+        }
+    }
+
+    if c_m1%2 {
+        for i in 0..r_m1 {
+            for j in 0..
+        }
+    }
 
     c
 }
