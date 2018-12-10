f7d98f561c9f4d04853f566f450ea08edd8f29fa
kam16u810 <bokseng12@gmail.com>
2018-12-10 17:54:41 +0300

    add working winograd algorithm

--- lab_05/src/winograd.rs
+++ lab_05/src/winograd.rs
@@ -6,23 +6,14 @@ pub fn winograd(m1: Vec<Vec<i32>>, m2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let c_m2 = m2[0].len();
     println!("m2 : {}x{}", r_m2, c_m2);
 
-    let mut c = Vec::new();
-
-    for _i in 0..r_m1 {
-        c.push(Vec::new());
-    }
-    for mut i in &mut c {
-        for _j in 0..c_m2 {
-            i.push(0);
-        }
-    }
+    let mut c = vec![vec![0; c_m2]; r_m1];
 
     let d = c_m1 / 2;
 
     let mut rows = vec![0; r_m1];
     println!("Here's array");
     for i in 0..r_m1 {
-        rows[i] += m1[i][0] + m1[i][1];
+        rows[i] += m1[i][0] * m1[i][1];
         for j in 1..d {
             rows[i] += m1[i][2*j] * m1[i][2*j+1];
         }
@@ -30,9 +21,9 @@ pub fn winograd(m1: Vec<Vec<i32>>, m2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     
     let mut columns = vec![0; c_m2];
     for i in 0..c_m2 {
-        columns[i] += m2[0][i]*m2[1][i];
+        columns[i] += m2[0][i] * m2[1][i];
         for j in 1..d {
-            columns[i] += m2[2*j][i]*m2[2*j+1][i];
+            columns[i] += m2[2*j][i] * m2[2*j+1][i];
         }
     }
 
@@ -46,9 +37,11 @@ pub fn winograd(m1: Vec<Vec<i32>>, m2: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         }
     }
 
-    if c_m1%2 {
+    if c_m1%2 == 1 {
         for i in 0..r_m1 {
-            for j in 0..
+            for j in 0..c_m2 {
+                c[i][j] += m1[i][c_m1-1] * m2[c_m1-1][j];
+            }
         }
     }
 
