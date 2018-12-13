fdab5eb9a294ddcddcf094e0fd9e67816cdfdd5a
kam16u810 <bokseng12@gmail.com>
2018-12-13 05:19:44 +0300

    add time measuring

--- lab_05/src/main.rs
+++ lab_05/src/main.rs
@@ -7,23 +7,22 @@ mod functions;
 use functions::*;
 use winograd::*;
 use standard::*;
+use std::time::Instant;
 
 static NTHREADS: usize = 4;
 
 fn main() {
-    let m1 = create_random_matrix(6, 6);
-    println!("This is the first random matrix: ");
-    //print_matrix(&m1);
+    let m1 = create_random_matrix(400, 400);
 
-    let m2 = create_random_matrix(6, 6);
-    println!("This is the second random matrix: ");
-    //print_matrix(&m2);
+    let m2 = create_random_matrix(400, 400);
     
+    let mut instant = Instant::now();
     let mut _2d = winograd(&m1, &m2, NTHREADS);
-    println!("Result matrix(winograd) is: ");
-    //print_matrix(&_2d);
+    let mut time = instant.elapsed();
+    println!("Time of winograd {:?} with {} threads", time, NTHREADS);
 
+    instant = Instant::now();
     _2d = standard(&m1, &m2, NTHREADS);
-    println!("Result matrix(standard) is: ");
-    //print_matrix(&_2d);
+    time = instant.elapsed();
+    println!("Time of winograd {:?} with {} threads", time, NTHREADS);
 }
