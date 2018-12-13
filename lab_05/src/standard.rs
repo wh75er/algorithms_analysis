39aeed5f57e6b6deecda3f0133861690b584d9ef
kam16u810 <bokseng12@gmail.com>
2018-12-13 04:48:41 +0300

    add parallelism in standard algorithm

--- lab_05/src/standard.rs
+++ lab_05/src/standard.rs
@@ -1,5 +1,8 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use crossbeam::thread;
+
+struct Shitpointer(*mut Vec<i32>);
+unsafe impl Send for Shitpointer {}
 
 pub fn standard(m1: &Vec<Vec<i32>>, m2: &Vec<Vec<i32>>, nthreads: usize) -> Vec<Vec<i32>> {
     let r_m1 = m1.len();
@@ -19,15 +22,45 @@ pub fn standard(m1: &Vec<Vec<i32>>, m2: &Vec<Vec<i32>>, nthreads: usize) -> Vec<
                 amount of elements");
     }
 
+    let m1_arced = Arc::new(m1.clone());
+    let m2_arced = Arc::new(m2.clone());
+
     let mut c = vec![vec![0; c_m2]; r_m1];
-    
-    for i in 0..r_m1 {
-        for j in 0..c_m2 {
-            for k in 0..c_m1{
-                c[i][j] += m1[i][k] * m2[k][j]
+
+    let r_m1_shift = r_m1 / nthreads;
+    let mut r_m1_from = 0;
+    let mut r_m1_to = r_m1_shift;
+
+    thread::scope(|scope| {
+        let mut threads = vec![];
+        for m in 0..nthreads {
+            let m1_data = m1_arced.clone();
+            let m2_data = m2_arced.clone();
+            let c_ptr = Shitpointer(c.as_mut_ptr());
+            threads.push(
+            scope.spawn(move |_| unsafe {
+                for i in r_m1_from..r_m1_to {
+                    for j in 0..c_m2 {
+                        for k in 0..c_m1{
+                            (*c_ptr.0.offset(i as isize))[j] += 
+                                m1_data[i][k] * m2_data[k][j];
+                        }
+                    }
+                }
+            }));
+
+            r_m1_from = r_m1_to;                          // Rows shift for 1 matrix
+            r_m1_to += r_m1_shift;
+
+            if m+2 == nthreads {                          // Check if this is the last iteration;
+                r_m1_to = r_m1;
             }
         }
-    }
+
+        for thread in threads {
+            thread.join().unwrap()
+        }
+    }).unwrap();
     
     c
 }
