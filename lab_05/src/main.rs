e71bdf8f899c344f31ede116d0f209526206afaf
kam16u810 <bokseng12@gmail.com>
2018-12-14 13:38:30 +0300

    add args parsing

--- lab_05/src/main.rs
+++ lab_05/src/main.rs
@@ -8,13 +8,19 @@ use functions::*;
 use winograd::*;
 use standard::*;
 use std::time::Instant;
-
-static NTHREADS: usize = 4;
+use std::env;
 
 fn main() {
-    let m1 = create_random_matrix(400, 400);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 || args.len() > 4 {
+        panic!("\nExpected 2 args:\n\tNTHREADS\n\tSize of square matrix\n");
+    }
+    let NTHREADS : usize = args[1].parse().unwrap();
+    let size : i32 = args[2].parse().unwrap();
 
-    let m2 = create_random_matrix(400, 400);
+    let m1 = create_random_matrix(size, size);
+
+    let m2 = create_random_matrix(size, size);
     
     let mut instant = Instant::now();
     let mut _2d = winograd(&m1, &m2, NTHREADS);
@@ -24,5 +30,5 @@ fn main() {
     instant = Instant::now();
     _2d = standard(&m1, &m2, NTHREADS);
     time = instant.elapsed();
-    println!("Time of winograd {:?} with {} threads", time, NTHREADS);
+    println!("Time of standard {:?} with {} threads", time, NTHREADS);
 }
