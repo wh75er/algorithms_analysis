ebfe81244131e9ad504854f822f6c674f7f518a6
kam16u810 <bokseng12@gmail.com>
2018-12-16 17:07:36 +0300

    change shell script vars

--- lab_05/test.sh
+++ lab_05/test.sh
@@ -3,9 +3,9 @@ echo starting...
 DIR='results_for_8'
 
 rm ${DIR}
-for j in 8; do
-    for i in {101..1001..100}; do
-        target/debug/lab_05 ${j} ${i} >> ${DIR}
+for i in 8; do
+    for j in {101..1001..100}; do
+        target/debug/lab_05 ${i} ${j} >> ${DIR}
         echo ${j}x${j} ${i} passed
     done
 
